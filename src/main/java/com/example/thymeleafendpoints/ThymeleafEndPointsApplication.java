dadf87a8811e29865f9a83886dbfdc3a7ddd3058
NurullahRumi <nurullahrumi@gmail.com>
2023-01-14 19:18:57 +0600

    First commit after create the project

---
+++ src/main/java/com/example/thymeleafendpoints/ThymeleafEndPointsApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafendpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafEndPointsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafEndPointsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thymeleafendpoints/ThymeleafEndPointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafendpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafEndPointsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
